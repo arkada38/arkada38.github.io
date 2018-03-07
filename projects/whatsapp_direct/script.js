879e68c2574c8633020f068ed959289c22eafa90
Arkadiy <arkada38@outlook.com>
2018-03-07 19:45:43 +0700

    Adding WhatsApp Direct

---
+++ projects/whatsapp_direct/script.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    tel: localStorage.getItem('tel') === null ? '' : localStorage.getItem('tel'),
+    message: localStorage.getItem('message') === null ? 'Здравствуйте, ' : localStorage.getItem('message')
+  },
+  computed: {
+    tel_numeric: function () {
+      var numbers = this.tel.replace(/\D/g,'')
+      
+      if (numbers.charAt(0) == "0")
+        numbers = numbers.replace("0", "66")
+
+      return numbers
+    },
+    message_encoded: function () {
+      return encodeURI(this.message).replace(/%5B/g, '[').replace(/%5D/g, ']')
+    }
+  },
+  methods: {
+    send: function () {
+      window.location.href = 'https://api.whatsapp.com/send?phone=' + this.tel_numeric + '&text=' + this.message_encoded
+    },
+    saveTel: function() {
+      localStorage.setItem('tel', this.tel)
+    },
+    saveMessage: function() {
+      localStorage.setItem('message', this.message)
+    },
+    clearTel: function () {
+      this.tel = ''
+      this.saveTel()
+    },
+    clearMessage: function () {
+      this.message = ''
+      this.saveMessage()
+    }
+  }
+})
