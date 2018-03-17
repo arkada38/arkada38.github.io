892df9493b9eca028239cd88628e911e6027c296
Arkadiy <arkada38@outlook.com>
2018-03-17 08:54:45 +0700

    Added more templates

--- projects/whatsapp_direct/scripts/script.js
+++ projects/whatsapp_direct/scripts/script.js
@@ -2,7 +2,9 @@ var app = new Vue({
   el: '#app',
   data: {
     tel: localStorage.getItem('tel') === null ? '' : localStorage.getItem('tel'),
-    message: localStorage.getItem('message') === null ? 'Здравствуйте, ' : localStorage.getItem('message')
+    messages: localStorage.getItem('messages') === null ? ['Здравствуйте, ', 'Здравствуйте, ', 'Здравствуйте, ', 'Здравствуйте, ', 'Здравствуйте, '] :
+      JSON.parse(localStorage.getItem('messages')),
+    active_tab: localStorage.getItem('active_tab') === null ? '0' : localStorage.getItem('active_tab')
   },
   computed: {
     tel_numeric: function () {
@@ -13,35 +15,47 @@ var app = new Vue({
 
       return numbers
     },
+    active_message: {
+      get: function () {
+        return this.messages[this.active_tab]
+      },
+      set: function (newValue) {
+        Vue.set(this.messages, this.active_tab, newValue)
+      }
+    },
     message_encoded: function () {
-      return encodeURI(this.message).replace(/%5B/g, '[').replace(/%5D/g, ']')
+      return encodeURI(this.messages[this.active_tab]).replace(/%5B/g, '[').replace(/%5D/g, ']')
     },
     whatsapp_link: function () {
       return 'https://api.whatsapp.com/send?phone=' + this.tel_numeric + '&text=' + this.message_encoded
     },
     country: function () {
       var data = new libphonenumber.parse('+' + this.tel_numeric)
-      
+
       if (data.country == undefined)
         return ''
 
-      return data.country + ' - ' + getCountryName(data.country)
+      return '<span class="badge badge-secondary">' + data.country + '</span> - <span class="badge badge-light">' + getCountryName(data.country) + '</span>'
     }
   },
   methods: {
     saveTel: function () {
       localStorage.setItem('tel', this.tel)
     },
-    saveMessage: function () {
-      localStorage.setItem('message', this.message)
+    saveMessages: function () {
+      localStorage.setItem('messages', JSON.stringify(this.messages))
     },
     clearTel: function () {
       this.tel = ''
       this.saveTel()
     },
-    clearMessage: function () {
-      this.message = ''
-      this.saveMessage()
+    clearActiveMessage: function () {
+      this.active_message = ''
+      this.saveMessages()
+    },
+    setActiveTab: function (n) {
+      this.active_tab = n
+      localStorage.setItem('active_tab', n)
     }
   }
 })
